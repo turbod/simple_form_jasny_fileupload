40f69da104a0b2b0ba451d58bd9d6462025d6892
Norbert Toth <tothnorbert.zalalovo@gmail.com>
2016-01-01 22:22:02 +0100

    Fix typo

--- lib/simple_form_jasny_fileupload.rb
+++ lib/simple_form_jasny_fileupload.rb
@@ -1,6 +1,5 @@
 require 'simple_form'
-require 'simple_form_jasny_fileupload/attachment_preview_input'
-#require 'simple_form_jasny_fileupload/image_preview_input'
+require 'simple_form_jasny_fileupload/jasnyfileinput_input'
 
 module SimpleFormJasnyFileupload
 end
